c732829034a5cc11c93fc2c159cfae990b6f4e13
Abdulsalam Amenov <amenov.abdulsalam@gmail.com>
2022-09-02 18:40:15 +0600

    update

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ import connectToAMQP from './lib/connect-to-amqp';
 import env from './lib/env';
 import getIncludeAliasesByOrderPrefixes from './lib/get-include-aliases-by-order-prefixes';
 import getModels from './lib/get-models';
+import getRedisClient from './lib/get-redis-client';
 import pluralize from './lib/pluralize';
 import setGlobalStructs from './lib/set-global-structs';
 declare const _default: {
@@ -9,6 +10,7 @@ declare const _default: {
     env: typeof env;
     getIncludeAliasesByOrderPrefixes: typeof getIncludeAliasesByOrderPrefixes;
     getModels: typeof getModels;
+    getRedisClient: typeof getRedisClient;
     pluralize: typeof pluralize;
     setGlobalStructs: typeof setGlobalStructs;
 };
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const connect_to_amqp_1 = __importDefault(require("./lib/connect-to-amqp"));
 const env_1 = __importDefault(require("./lib/env"));
 const get_include_aliases_by_order_prefixes_1 = __importDefault(require("./lib/get-include-aliases-by-order-prefixes"));
 const get_models_1 = __importDefault(require("./lib/get-models"));
+const get_redis_client_1 = __importDefault(require("./lib/get-redis-client"));
 const pluralize_1 = __importDefault(require("./lib/pluralize"));
 const set_global_structs_1 = __importDefault(require("./lib/set-global-structs"));
 exports.default = {
@@ -14,6 +15,7 @@ exports.default = {
     env: env_1.default,
     getIncludeAliasesByOrderPrefixes: get_include_aliases_by_order_prefixes_1.default,
     getModels: get_models_1.default,
+    getRedisClient: get_redis_client_1.default,
     pluralize: pluralize_1.default,
     setGlobalStructs: set_global_structs_1.default
 };
